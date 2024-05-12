2e3d05ffec61a238a93d26fd0f347dea6ebfc263
alimem09 <nurmanovali56@gmail.com>
2024-05-12 12:58:14 +0500

    Sigma47

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
 let button_start = document.querySelector(".button")
-
+let isbrush = true;
 button_start.addEventListener("click",()=>{
     let main = document.querySelector(".main-page")
     let start = document.querySelector(".start-page")
@@ -14,18 +14,41 @@ const ctx = canvas.getContext('2d');
 let isDrawing = false;
 
 canvas.onmousedown = ({offsetX, offsetY}) => {
-    isDrawing = true;
-    ctx.beginPath();
-    ctx.moveTo(offsetX, offsetY);
+    if (isbrush = true)
+        {isDrawing = true;
+        ctx.beginPath();
+        ctx.moveTo(offsetX, offsetY);}
+    
 };
 
 canvas.onmousemove = ({offsetX, offsetY}) => {
-    if (isDrawing) {
+    
+    if (isbrush = true){if (isDrawing) {
         ctx.lineTo(offsetX, offsetY);
-        ctx.stroke();
+        ctx.stroke();}
+        
     }
 };
 
 canvas.onmouseup = () => {
     isDrawing = false;
 };
+const colorPicker = document.getElementById('colorPicker');
+
+ctx.strokeStyle = colorPicker.value;
+
+
+colorPicker.addEventListener('change', () => ctx.strokeStyle = colorPicker.value);
+
+document.getElementById('eraserBtn').addEventListener('click', () => {
+    ctx.globalCompositeOperation = 'destination-out';
+    ctx.lineWidth = lineWidth.value;
+    isbrush = false;
+})
+const lineWidth = document.getElementById('lineWidth');
+
+ctx.lineWidth = lineWidth.value;
+
+lineWidth.addEventListener('change', () => ctx.lineWidth = lineWidth.value);
+let brush = document.querySelector(".crush")
+brush.addEventListener("click",()=>isbrush = true)
